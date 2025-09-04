de63cddde226afbfe8c8b14dd11a03f7dcdfb27f
Jefferson Nascimento <123571211+finholuiz@users.noreply.github.com>
2025-09-04 08:59:43 -0300

    Add files via upload

---
+++ projeto_banco.py
@@ -0,0 +1,70 @@
+menu = """
+==========================================
+      BEM-VINDO AO BANCO NASCIMENTO
+==========================================
+
+[0] Depositar
+[1] Sacar
+[2] Extrato
+[3] Sair
+
+==========================================
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 5
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "0":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "1":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Você não tem saldo suficiente para Sacar.")
+
+        elif excedeu_limite:
+            print("O valor do saque excede o limite diario.")
+
+        elif excedeu_saques:
+            print("Número máximo de saques excedido, por dia.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("O valor informado é inválido. Tente Novamente.")
+
+    elif opcao == "2":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações, até o momento." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "3":
+        break
+
+    else:
+        print("Por favor selecione novamente a operação desejada.")
